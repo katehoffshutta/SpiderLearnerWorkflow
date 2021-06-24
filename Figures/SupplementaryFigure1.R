8623a60ce15b8ced285f900cdc9565a724f6bcc0
Kate Hoff Shutta <kshutta@umass.edu>
2021-06-23 22:03:58 -0400

    switch S1 and S2 fig names

--- Figures/SupplementaryFigure1.R
+++ Figures/SupplementaryFigure1.R
@@ -1,24 +1,10 @@
-library(huge)
-library(igraph)
-library(moments)
-library(pracma)
+unbd = read.csv("../Simulations/ensModelWeightsUnbd.csv")
+bd = read.csv("../Simulations/ensModelWeightsBd.csv")
 
-#### Get discrete uniform distribution from CATHGEN Example ####
-standardize = function(x) return((x-mean(x))/sd(x))
+methods = c("glasso-ebic-0","glasso-ebic-0.5","glasso-ric","hglasso","mle")
 
-cathgen = as.matrix(read.table("~/umass/research/networks/metabolomics2019/Biostat-study-master/Data/data.txt",sep="\t", header=T))
-cathgen=apply(cathgen,2,standardize) # gave same results - glasso standardizes
-cathgenGGM = huge(cathgen, method="glasso")
-cathgenOptGGM = huge.select(cathgenGGM, criterion="ebic",ebic.gamma=0)
-cathgenEdgeWeights = c(-cov2cor(cathgenOptGGM$opt.icov)+diag(ncol(cathgen)))
-cathgenNonZeroEdgeWeights = cathgenEdgeWeights[cathgenEdgeWeights != 0]
-cathgenHist = hist(cathgenNonZeroEdgeWeights,breaks=100)
-
-pdf("cathgenPrecisionDist.pdf",width=8,height=8)
-hist((cathgenOptGGM$opt.icov- diag(cathgenOptGGM$opt.icov))[which(cathgenOptGGM$opt.icov- diag(cathgenOptGGM$opt.icov) != 0)],
-     breaks=100,xlab="Precision Matrix Entries", main="CATHGEN Off-diagonal Precision Matrix Distribution")
+pdf("boundedLoss2.pdf",width=20,height=8)
+par(mfrow=c(1,2))
+boxplot(unbd,names=methods,ylab="weights",main="Unbounded Loss Function Weights")
+boxplot(bd,names=methods,ylab="weights",main="Bounded Loss Function Weights")
 dev.off()
-
-skewness(cathgenNonZeroEdgeWeights)
-kurtosis(cathgenNonZeroEdgeWeights)
-
--- Figures/SupplementaryFigure2.R
+++ Figures/SupplementaryFigure2.R
@@ -1,10 +1,24 @@
-unbd = read.csv("../Simulations/ensModelWeightsUnbd.csv")
-bd = read.csv("../Simulations/ensModelWeightsBd.csv")
+library(huge)
+library(igraph)
+library(moments)
+library(pracma)
 
-methods = c("glasso-ebic-0","glasso-ebic-0.5","glasso-ric","hglasso","mle")
+#### Get discrete uniform distribution from CATHGEN Example ####
+standardize = function(x) return((x-mean(x))/sd(x))
 
-pdf("boundedLoss2.pdf",width=20,height=8)
-par(mfrow=c(1,2))
-boxplot(unbd,names=methods,ylab="weights",main="Unbounded Loss Function Weights")
-boxplot(bd,names=methods,ylab="weights",main="Bounded Loss Function Weights")
+cathgen = as.matrix(read.table("~/umass/research/networks/metabolomics2019/Biostat-study-master/Data/data.txt",sep="\t", header=T))
+cathgen=apply(cathgen,2,standardize) # gave same results - glasso standardizes
+cathgenGGM = huge(cathgen, method="glasso")
+cathgenOptGGM = huge.select(cathgenGGM, criterion="ebic",ebic.gamma=0)
+cathgenEdgeWeights = c(-cov2cor(cathgenOptGGM$opt.icov)+diag(ncol(cathgen)))
+cathgenNonZeroEdgeWeights = cathgenEdgeWeights[cathgenEdgeWeights != 0]
+cathgenHist = hist(cathgenNonZeroEdgeWeights,breaks=100)
+
+pdf("cathgenPrecisionDist.pdf",width=8,height=8)
+hist((cathgenOptGGM$opt.icov- diag(cathgenOptGGM$opt.icov))[which(cathgenOptGGM$opt.icov- diag(cathgenOptGGM$opt.icov) != 0)],
+     breaks=100,xlab="Precision Matrix Entries", main="CATHGEN Off-diagonal Precision Matrix Distribution")
 dev.off()
+
+skewness(cathgenNonZeroEdgeWeights)
+kurtosis(cathgenNonZeroEdgeWeights)
+
